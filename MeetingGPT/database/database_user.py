1b490b8662725c53d1459b261b95d9f5e262afcb
IsaqueZS93 <isaquezeferino.silva@gmail.com>
2025-01-19 22:32:53 -0300

    Update database_user.py

--- MeetingGPT/database/database_user.py
+++ MeetingGPT/database/database_user.py
@@ -10,12 +10,7 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s'
 )
 
-# Configuração inicial do logger
-logging.basicConfig(
-    filename='database_meeting.log',
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
+
 
 class DatabaseUser:
     def __init__(self):
